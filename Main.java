069a3d0ada6be038be305f094a288791305e906c
vishal-panchal611 <vpanchal611@gmail.com>
2024-10-02 13:42:59 +0530

    Adding 3 files for Quiz

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Quiz;
+
+public class Main {
+    public static void main(String[] args) {
+
+        QuestionService service = new QuestionService();
+        service.playQuiz();
+        service.printScore();
+
+    }
+}
